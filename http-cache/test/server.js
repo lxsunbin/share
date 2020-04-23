8c9d1dfdb8fcc639672a7a12ea9826c1346b65b3
lxsunbin <lxsunbin@qq.com>
2020-04-23 16:14:28 +0800

    update

--- http-cache/test/server.js
+++ http-cache/test/server.js
@@ -2,37 +2,41 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer(function (request, response) {
-	console.log('request come', request.url);
+    console.log('request come', request.url);
 
-	if (request.url === '/') {
-		const html = fs.readFileSync('test.html', 'utf8');
-		response.writeHead(200, {
-			'Content-Type': 'text/html'
-		});
-		response.end(html);
-	}
+    if (request.url === '/') {
+        const html = fs.readFileSync('test.html', 'utf8');
+        response.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        response.end(html);
+    }
 
-	if (request.url === '/script.js') {
-		response.writeHead(200, {
-			'Content-Type': 'text/javascript',
-	        'Cache-Control': 'max-age=10'
-		});
-		response.end('console.log("script loaded")');
-	}
+    if (request.url === '/script.js') {
+    	response.writeHead(200, {
+    		'Content-Type': 'text/javascript',
+            'Cache-Control': 'max-age=10'
+    	});
+    	response.end('console.log("script loaded")');
+    }
 
-	// const etag = request.headers['if-none-match'];
-	// if (etag === '777') {
-	// 	response.writeHead(304, {
-	// 		'Content-Type': 'text/javascript',
-	// 		'Etag': '777'
-	// 	});
-    //     response.end();
-	// } else {
-	// 	response.writeHead(200, {
-	// 		'Content-Type': 'text/javascript',
-	// 		'Etag': '777'
-	// 	});
-	// 	response.end('console.log("script loaded")');
+    // if (request.url === '/script.js') {
+    //     const etag = request.headers['if-none-match'];
+    //     if (etag === '777') {
+    //         response.writeHead(304, {
+    //             'Content-Type': 'text/javascript',
+    //             'Cache-Control': 'no-cache',
+    //             'Etag': '777'
+    //         });
+    //         response.end();
+    //     } else {
+    //         response.writeHead(200, {
+    //             'Content-Type': 'text/javascript',
+    //             'Cache-Control': 'no-cache',
+    //             'Etag': '777'
+    //         });
+    //         response.end('console.log("script loaded")');
+    //     }
     // }
 }).listen(3000);
 
